946846210afbf903821bb5ab577e699e558ffbce
DevonBar <barangandevon@gmail.com>
2025-11-23 12:24:39 +0800

    chore: put and delete method for fast api

--- index.py
+++ index.py
@@ -48,3 +48,29 @@ def create_user(user_id:int, user:User):
     
     users[user_id] = user.model_dump()
     return user
+
+@app.put("/users/${user_id}")
+def update_user(user_id:int, user:UpdateUser):
+    if user_id not in users:
+        raise HTTPException(status_code=404, detail="User is not here")
+    
+    current_user = users[user_id]
+
+    if user.name is not None:
+        current_user["name"] = user.name
+    if user.age is not None:
+        current_user["age"] = user.age
+    if user.role is not None:
+        current_user["role"] = user.role
+    
+    return current_user
+
+
+
+@app.delete("/users/${user_id}")
+def delete_user(user_id:int):
+    if user_id not in users:
+        raise HTTPException(status_code=404, detail="No user here")
+    
+    delete_user = users.pop(user_id)
+    return {"message":"User has been deleted","Deleted user": delete_user}
